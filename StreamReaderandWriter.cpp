ca7a8e197479b73eb339abcceaeb56080f78595c
rayhanramadhan17 <rayhanramadhan179072@gmail.com>
2026-06-01 18:23:11 +0700

    menutup file membuka file dan menunjuk file

--- StreamReaderandWriter.cpp
+++ StreamReaderandWriter.cpp
@@ -29,4 +29,12 @@ int main()
         outfile << baris << endl;
         
     }
-    
+    //menutup file setelah penulisan selesai
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
